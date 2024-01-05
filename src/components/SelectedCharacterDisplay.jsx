02b8d4d833964c6161fe9c57c77d36c741f2cd04
ayzel <andrewzlee@gmail.com>
2024-01-05 17:35:32 -0500

    update filename system

--- src/components/SelectedCharacterDisplay.jsx
+++ src/components/SelectedCharacterDisplay.jsx
@@ -11,14 +11,14 @@ const SelectedCharacters = ({ selectedCharacterList, onPortraitClick, options })
     // download as png
     htmlToImage.toPng(node)
       .then((dataUrl) => {
-        downloadjs(dataUrl, 'team.png');
+        downloadjs(dataUrl, options['teamName'] + '.png');
       })
     :
     
     // download as svg
     htmlToImage.toSvg(node)
       .then((dataUrl) => {
-        downloadjs(dataUrl, 'team.svg');
+        downloadjs(dataUrl, options['teamName'] + '.svg');
       })
   }
 
@@ -33,6 +33,7 @@ const SelectedCharacters = ({ selectedCharacterList, onPortraitClick, options })
     <div className='bg-blue-950 flex flex-col'>
       <h1 className='text-4xl font-semibold text-white px-5 pt-5'>Selected Characters</h1>
       <div id='capture-box' className='inline-block p-5'>
+        {charactersExist ?
         <div className={displayBoxOptions}>
           {(charactersExist && options['teamName'].length > 0) && <h1 className='text-3xl font-semibold text-white px-8 pt-5'>{options['teamName']}</h1>}
           <div className='flex flex-row gap-4 p-8 items-center'> {/* selected characters panel */}
@@ -49,6 +50,11 @@ const SelectedCharacters = ({ selectedCharacterList, onPortraitClick, options })
             ))}
           </div>
         </div>
+        :
+        <div className='flex items-center justify-center h-80'>
+          <h1 className='text-white text-2xl font-mono bg-slate-800 p-16 rounded-xl'>Team will appear here!</h1>
+        </div>
+        }
       </div>
       {charactersExist &&
       <div className='block'>
